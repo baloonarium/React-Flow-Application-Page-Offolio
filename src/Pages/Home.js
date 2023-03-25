49e301101fbee6e85a556e48f87f6326735d7a02
Gregoire Bigot <gregoirebigot@macbook-pro-de-gregoire.home>
2023-03-25 10:07:54 +0100

    Change page / home to connect with My Library app

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className='MyLibrary'>
+           <LeftNav/>
+           <Header/>
+           <NavApp />
+           <Flow />
         </div>
     );
 };
